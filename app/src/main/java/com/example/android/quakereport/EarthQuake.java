4d4b06aa5888c230ba963a66568d6cbd1e70799e
JAPM <cicelcup@gmail.com>
2019-06-13 14:09:57 -0600

    Creating the EarthQuakeAdapter, changing the constructor of the EarthQuake Class and adding comments to the layouts

--- app/src/main/java/com/example/android/quakereport/EarthQuake.java
+++ app/src/main/java/com/example/android/quakereport/EarthQuake.java
@@ -1,12 +1,19 @@
+/*Class where is storage all the earthquake information*/
+
 package com.example.android.quakereport;
 
 public class EarthQuake {
+    //Magnitude of earthquake
     private double mMagnitude;
+    //Place of earthquake
     private String mPlace;
+    //Date of earthquake
     private int mDate;
 
-    public EarthQuake(String mPlace) {
+    public EarthQuake(double mMagnitude, String mPlace, int mDate) {
+        this.mMagnitude = mMagnitude;
         this.mPlace = mPlace;
+        this.mDate = mDate;
     }
 
     public double getMagnitude() {
---
+++ app/src/main/java/com/example/android/quakereport/EarthQuakeAdapter.java
@@ -0,0 +1,47 @@
+package com.example.android.quakereport;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class EarthQuakeAdapter extends ArrayAdapter<EarthQuake> {
+
+    //Constructor for the earthquake
+    public EarthQuakeAdapter(Context context, int resource, List<EarthQuake> earthQuakeList) {
+        super(context, 0, earthQuakeList);
+    }
+
+    //get item of the adapter
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View currentView = convertView;
+
+        if (currentView == null) {
+            //if it's not created, inflate the layout
+            currentView = LayoutInflater.from(getContext()).
+                    inflate(R.layout.earthquake_information, parent);
+        }
+
+        //get the current position from the adapter
+        EarthQuake earthQuake = getItem(position);
+
+        //Set the magnitude text
+        TextView textViewMagnitude = currentView.findViewById(R.id.magnitude);
+        textViewMagnitude.setText(Double.toString(earthQuake.getMagnitude()));
+
+        //set the place text
+        TextView textViewPlace = currentView.findViewById(R.id.place);
+        textViewPlace.setText(earthQuake.getPlace());
+
+        //set the date text
+        TextView textViewDate = currentView.findViewById(R.id.date);
+        textViewDate.setText(Integer.toString(earthQuake.getDate()));
+
+        return currentView;
+    }
+}
