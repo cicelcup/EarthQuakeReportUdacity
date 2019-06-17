d12f891090df47d413c02250e5d0fc304b39dc93
JAPM <cicelcup@gmail.com>
2019-06-17 14:31:59 -0600

    Adding the QueryEarthQuake Class and changing the adapter creation for showing the data

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
-import java.util.ArrayList;
-
 public class EarthquakeActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
@@ -17,22 +15,12 @@ public class EarthquakeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquakelist);
 
-        // Create a fake list of earthquake locations.
-        ArrayList<EarthQuake> earthquakes = new ArrayList<EarthQuake>();
-
-        earthquakes.add(new EarthQuake(6.5, "San Francisco", "8 de Diciembre"));
-        earthquakes.add(new EarthQuake(3.5, "Miami", "6 de Julio"));
-        earthquakes.add(new EarthQuake(4.5, "San Jose", "8 de Febrero"));
-        earthquakes.add(new EarthQuake(5.5, "San Antonio", "18 de Marzo"));
-        earthquakes.add(new EarthQuake(7.5, "Los Angeles", "14 de Abril"));
-        earthquakes.add(new EarthQuake(3.5, "Alajuela", "2 de Enero"));
-        earthquakes.add(new EarthQuake(2.5, "Heredia", "6 de Mayo"));
-
         // Find a reference to the ListView
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
         // Create a new EarthQuakeAdapter
-        EarthQuakeAdapter adapter = new EarthQuakeAdapter(this, earthquakes);
+        EarthQuakeAdapter adapter = new EarthQuakeAdapter(this,
+                QueryEarthQuake.extractEarthquakes());
 
         // Set the adapter
         earthquakeListView.setAdapter(adapter);
