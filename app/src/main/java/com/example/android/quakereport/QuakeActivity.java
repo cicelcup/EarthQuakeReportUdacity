5d328b718d369d9cf383e4d3f2093ae2b697a070
JAPM <cicelcup@gmail.com>
2019-06-19 15:03:05 -0600

    Indicating the permission for Internet into the manifest file and creating the thread for requesting the information

--- app/src/main/java/com/example/android/quakereport/QuakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/QuakeActivity.java
@@ -4,11 +4,15 @@ package com.example.android.quakereport;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+
+import java.net.URL;
+import java.util.ArrayList;
 
 public class QuakeActivity extends AppCompatActivity {
 
@@ -27,6 +31,8 @@ public class QuakeActivity extends AppCompatActivity {
         // Set the adapter
         quakeListView.setAdapter(adapter);
 
+        //Set the click into an item of the list
+
         quakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -37,4 +43,17 @@ public class QuakeActivity extends AppCompatActivity {
             }
         });
     }
+
+    private class RequestQuake extends AsyncTask<URL, Void, ArrayList<Quake>> {
+        @Override
+        protected ArrayList<Quake> doInBackground(URL... urls) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Quake> quakes) {
+            super.onPostExecute(quakes);
+        }
+    }
+
 }
