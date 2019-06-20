8a3f994e2eb77def21cf23a054060b2736f94e02
JAPM <cicelcup@gmail.com>
2019-06-20 10:56:21 -0600

    Updating the quakes list according the query

--- app/src/main/java/com/example/android/quakereport/QuakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/QuakeActivity.java
@@ -7,12 +7,12 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class QuakeActivity extends AppCompatActivity {
+
 
     //URL for the last 10 earthquake with more than 6 of magnitude
     private static final String USGS_REQUEST_URL =
@@ -29,12 +29,15 @@ public class QuakeActivity extends AppCompatActivity {
         //Executing the Thread
         quakeRequest.execute(USGS_REQUEST_URL);
 
+    }
+
+    void updateList(String jsonQuakes) {
         // Find a reference to the ListView
         ListView quakeListView = findViewById(R.id.quake_list);
 
         // Create a new QuakeAdapter
         final QuakeAdapter adapter = new QuakeAdapter(this,
-                QuakeQuery.extractQuakes());
+                QuakeQuery.extractQuakes(jsonQuakes));
 
         // Set the adapter
         quakeListView.setAdapter(adapter);
@@ -67,7 +70,6 @@ public class QuakeActivity extends AppCompatActivity {
             }
 
             quakes = QuakeUtils.fetchURL(urls[0]); //Requesting the information throught http
-            Log.i("JAPM", quakes);
 
             return quakes;
         }
@@ -75,7 +77,7 @@ public class QuakeActivity extends AppCompatActivity {
         //after the thread finish to execute execute this process in the UI thread
         @Override
         protected void onPostExecute(String quakes) {
-            super.onPostExecute(quakes);
+            updateList(quakes);
         }
     }
 
